d3c80c7686a68bad61b3db2502c1efe7cf443cab
kamal.elhattab <kamal.elhattab@nexusat.it>
2018-05-28 13:14:53 +0200

    kamal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector:'.mine', //tag 'my_root', attibute of other component  '[mine]',  class .css 
+  
+// le nom de la balise pour inserer le project dans le fichier html
+  // si je met le nom entre [] cet elemet n'est plus inseré comme une balise mais comme un attibut dìun conteneur html: DIV par exemple
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
